701a25a4ebe6ea68e3be4c0e18ad3d53255580b2
SMAN6informatika <sman6informatika@gmail.com>
2024-07-10 14:23:00 +0700

    first commit

---
+++ quiz/assets/js/game.js
@@ -0,0 +1,125 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+    question: "Benua apa yang disebut benua terbesar di dunia?",
+    choice1: "Benua Asia",
+    choice2: "Benua Afrika",
+    choice3: "Benua Eropa",
+    choice4: "Benua Australia",
+    answer: 1
+  },
+  {
+    question:
+      "Benua apa yang dijuluki sebagai Benua hitam? ?",
+    choice1: "Benua Asia",
+    choice2: "Benua Australia",
+    choice3: "Benua Afrika",
+    choice4: "Benua Eropa",
+    answer: 3
+  },
+  {
+    question: " Indonesia dijuluki sebagai...?",
+    choice1: "Negara Bambu",
+    choice2: "Negara Sakura",
+    choice3: "Negara Hitam",
+    choice4: "Negara Maritim",
+    answer: 4
+  }
+];
+
+//CONSTANTS
+const INCORRECT_TAX = 10;
+const MAX_QUESTIONS = 3;
+
+// Start Game & Timer
+startGame = () => {
+  questionCounter = 0;
+  score = 100;
+  availableQuesions = [...questions];
+  getNewQuestion();
+
+  // Timer
+  setInterval(function () {
+    score--;
+    scoreText.innerText = score;
+
+    if (score === 0) {
+      localStorage.setItem("mostRecentScore", score);
+
+      //go to the end page
+      return window.location.assign("../../assets/html/end.html");
+    }
+  }, 1000);
+};
+
+// Display Next Random Question and Answers
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+
+    //go to the end page
+    return window.location.assign("../html/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  // Get Answers
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+//Get User's Choice
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "incorrect") {
+      decrementScore(INCORRECT_TAX);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+//Penalty for wrong choice
+decrementScore = num => {
+  score -= num;
+  scoreText.innerText = score;
+};
+
+
+startGame();
